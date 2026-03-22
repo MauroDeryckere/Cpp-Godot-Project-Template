aa522e0cfa51aaa054dec4897fe694f2c5190a1c
Mauro Deryckere <mauro.deryckere@gmail.com>
2026-03-22 16:39:25 +0100

    Improved cpp example

--- cpp/include/godot/MyNode.hpp
+++ cpp/include/godot/MyNode.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/variant/string.hpp>
 
 namespace godot
 {
@@ -12,9 +13,17 @@ namespace godot
         MyNode() = default;
         ~MyNode() = default;
 
-        void _process(double delta);
+        void _ready() override;
+        void _process(double delta) override;
+
+        void set_speed(double speed);
+        double get_speed() const;
 
     protected:
         static void _bind_methods();
+
+    private:
+        double m_Speed = 1.0;
+        double m_TimeElapsed = 0.0;
     };
 }
--- cpp/src/godot/MyNode.cpp
+++ cpp/src/godot/MyNode.cpp
@@ -6,14 +6,41 @@
 
 using namespace godot;
 
+void MyNode::_ready()
+{
+    UtilityFunctions::print("MyNode ready with speed: ", m_Speed);
+}
+
 void MyNode::_process(double delta)
 {
     engine::Time::Update(delta);
 
-    UtilityFunctions::print(engine::Time::GetDelta());
+    m_TimeElapsed += delta * m_Speed;
+
+    if (m_TimeElapsed >= 1.0)
+    {
+        m_TimeElapsed -= 1.0;
+        emit_signal("tick");
+    }
+}
+
+void MyNode::set_speed(double speed)
+{
+    m_Speed = speed;
+}
+
+double MyNode::get_speed() const
+{
+    return m_Speed;
 }
 
 void MyNode::_bind_methods()
 {
-    // can be empty for now
+    // Property: speed (visible in the Godot inspector)
+    ClassDB::bind_method(D_METHOD("set_speed", "speed"), &MyNode::set_speed);
+    ClassDB::bind_method(D_METHOD("get_speed"), &MyNode::get_speed);
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "speed"), "set_speed", "get_speed");
+
+    // Signal: tick (emitted once per second, scaled by speed)
+    ADD_SIGNAL(MethodInfo("tick"));
 }
